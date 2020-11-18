2e2c0f03db3ec364bace3439cc4021e91ad751b1
George Svarovsky <gsvarovsky@gmail.com>
2020-11-18 10:19:25 +0000

    Latest m-ld prerelease

--- broker.js
+++ broker.js
@@ -3,7 +3,7 @@ const server = require('net').createServer(aedes.handle)
 const port = 1883
 
 server.listen(port, () => {
-  console.log('MQTT server started and listening on port ', port)
+  console.log('MQTT broker started and listening on port ', port)
   process.send('started')
 });
 
